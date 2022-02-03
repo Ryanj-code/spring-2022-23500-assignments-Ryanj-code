f7cb1ecbef0ebd9dafd062cde653ce24f975bbf7
Ryan Jiang <jiangr0421@gmail.com>
2022-02-02 22:25:54 -0500

    Edited comments

--- assignments/recursion/staircase.cpp
+++ assignments/recursion/staircase.cpp
@@ -18,8 +18,9 @@ int stepPerms(int n){
 } // Keeps calling stepPerms(n-1) + stepPerms(n-2) + stepPerms(n-3) because the number of ways to climb the stair for n > 3
 // Is equal to the sum of the previous 3 number's ways to climb the stairs.
 // Example: # of ways for 5 = 13, # of ways for 4 = 7, # of ways for 3 = 4, # of ways for 2 = 2.
-// Result: 13 = 7 + 4 + 2.        To get # of ways for 4; # of ways for 3 = 4, # of ways for 2 = 2, # of ways for 1 = 1.                       
-//                                Therefore, 7 = 4 + 2 + 1.
+// To get # of ways for 4; # of ways for 3 = 4, # of ways for 2 = 2, # of ways for 1 = 1.      
+// Therefore, 7 = 4 + 2 + 1.
+// Result: 13 = 7 + 4 + 2. # of ways for 5 steps is 13.                                                  
 
 int main() {
   std::cout << "# of ways: " << stepPerms(1) << std::endl;
