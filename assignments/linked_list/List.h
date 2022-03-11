4a84948a05da7d30963ac300e1aa62cbe3c4f1d6
Ryan Jiang <jiangr0421@gmail.com>
2022-03-10 19:22:59 -0500

    Base code for list.h

--- assignments/linked_list/List.h
+++ assignments/linked_list/List.h
@@ -1,15 +1,23 @@
 #pragma once
+
 #include "Node.h"
 
 class List{
- private:
-  Node *head;
+ private: Node *head;
 
  public:
   List();
   ~List();
+
   void insert(std::string data);
-  void remove(int index);
-  Node * locate(int index);
-  std::string toString();
+  void insert(int loc, std::string data);
+
+  void remove(int loc);
+  
+  std::string get(int loc);
+
+  int length();
+
+  std::string toString(); // for testing and debugging
+  
 };
