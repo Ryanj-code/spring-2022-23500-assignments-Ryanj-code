c500763f0e73393424e8f591183ce35147188dff
Ryan Jiang <jiangr0421@gmail.com>
2022-03-10 19:24:43 -0500

    Changed main.cpp

--- assignments/linked_list/main.cpp
+++ assignments/linked_list/main.cpp
@@ -1,57 +1,29 @@
 #include <iostream>
-#include "Node.h"
 #include "List.h"
+#include "Node.h"
 
-int main(int argc, char *argv[]){
-  /*
-  Node *p1 = new Node();
-  p1->setData("hello");
+int main()
+{
+  List *l = new List();
 
-  Node *p2 = new Node("world");
-  p1->setNext(p2);
-    
-  std::cout<< p1-> getData() << "\n";
-  std::cout<< p2-> getData() << "\n";
-  std::cout<< p1-> getNext() -> getData() << "\n";
-  
-  p2 = new Node("!");
-  p1-> getNext()->setNext(p2);
+  l->insert("zero");
+  l->insert("one");
+  l->insert("two");
+  l->insert("three");
+  l->insert("four");
 
-  std::cout<< p1-> getData() << "\n";
-  std::cout<< p1-> getNext() -> getData() << "\n";
-  std::cout<< p1-> getNext()-> getNext() -> getData() << "\n";
+  std::cout << l->toString() << "\n";
+  l->insert(3,"HellO");
+  std::cout << l->toString() << "\n";
+  l->insert(3,"World");
+  std::cout << l->toString() << "\n";
+  l->insert(0,"HellO");
+  std::cout << l->toString() << "\n";
+  l->insert(0,"front");
+  std::cout << l->toString() << "\n";
 
-  Node *walker = p1;
-  std::cout<< walker->getData() <<"\n";
-
-  walker = walker->getNext();//walker = p1->getNext();
-  std::cout<< walker->getData() <<"\n";
-
-  walker = walker->getNext();//walker = p1->getNext();
-  std::cout<< walker->getData() <<"\n";
-
-  walker = p1;
-  while(walker != nullptr){
-    std::cout<<walker->getData()<<"\n";
-    walker = walker->getNext();
-  }
-  */  
-  // At this point walker points to (refers to) nullptr
-  // and if we try to dereference it, we get a segfault.
-  // std::cout<<walker->getData()<<"\n";
-
-  List *l1 = new List();
-  std::cout << l1->toString() << "\n";
-    
-  l1->insert("a");
-  l1->insert("b");
-  l1->insert("c");
-  std::cout << l1->toString() << "\n";
-
-  std::cout << l1->locate(2) << ", " << l1->locate(2)->getData() << "\n";  
-
-  //l1->remove(1);
-  std::cout << l1->toString() << "\n";
-    
+  l->remove(1);
+  std::cout << l->toString() << "\n";
+  delete l;
   return 0;
 }
