2b82c32e1468cf28607bae2ba211fe3eafac1f3e
Ryan Jiang <jiangr0421@gmail.com>
2022-03-31 20:20:40 -0400

    added isPathtoFreedom code

--- assignments/pointer-maze/Labyrinth.cpp
+++ assignments/pointer-maze/Labyrinth.cpp
@@ -1,8 +1,34 @@
 #include "Labyrinth.h"
+#include <iostream>
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
-    /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
-    return false;
+  bool spellbook, potion, wand;
+  MazeCell * current = start;
+  
+  for(int i = 0; i < moves.length(); i++){
+    /*std::cout << current << "\n";*/
+    
+    if(current == nullptr) return false;
+    
+    if(moves[i] == 'N') current = current->north;
+    else if(moves[i] == 'S') current = current->south;
+    else if(moves[i] == 'W') current = current->west;
+    else if(moves[i] == 'E') current = current->east;
+    
+    if(current == nullptr) return false;
+
+    if(current != nullptr){
+      Item item = current->whatsHere;
+      switch(item){
+      case Item::SPELLBOOK : spellbook = true; break;
+      case Item::POTION : potion = true; break;
+      case Item::WAND : wand = true; break;
+      case Item::NOTHING : break;
+      }
+    }
+  }
+  
+  /*std::cout << spellbook << "\n" << potion << "\n" << wand << "\n";*/
+  
+  return(spellbook && potion && wand);
 }
