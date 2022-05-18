f6ffc4664db50b439abf201fce14e6c578aea919
Ryan Jiang <jiangr0421@gmail.com>
2022-05-17 21:26:35 -0400

    Added base files for hashing

---
+++ assignments/hashing/List.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Node.h"
+
+class List{
+ private:
+  Node *head;
+
+ public:
+  List();
+  ~List();
+  
+  void insert(Person *p);
+  std::string toString();
+  Node * locate(int index);
+  void remove(int index);
+  Node * get_head();
+};
---
+++ assignments/hashing/Node.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Person.h"
+
+class Node{
+ private:
+  Person *person;
+  Node *next;
+
+ public:
+  Node();
+  Node(Person *p);
+  ~Node();
+  Node(Person *p, Node *next);
+
+  void setPerson(Person *data);
+  void setNext(Node *next);
+
+  Person *getPerson();
+  Node *getNext();
+};
